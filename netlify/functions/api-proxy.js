a78eb7768a29c4828f41cae4a5a4327dc5134b24
jonmobley <62940677+jonmobley@users.noreply.github.com>
2024-10-01 10:34:07 -0400

    Update api-proxy.js

--- netlify/functions/api-proxy.js
+++ netlify/functions/api-proxy.js
@@ -1,1 +1,9 @@
+exports.handler = async function(event, context) {
+  // This is a placeholder function
+  // You'll need to customize this to work with your specific APIs
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Hello from the API proxy function!" })
+  };
+};
 
